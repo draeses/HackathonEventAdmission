d3301041649b9e67bd9a5ed86a41fe28f606da3d
Vanhee, Dree <dree.vanhee@capgemini.com>
2017-11-24 10:22:21 +0100

    the good project

---
+++ HackathonEventAdmission/LiveCameraSample/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveCameraSample
+{
+    static class Extensions
+    {
+        public static bool NoneEmpty(this List<Person> list)
+        {
+            bool check = false;
+            foreach (Person p in list)
+            {
+                if (p.ComparePerson(new Person()))
+                    check = true;
+            }
+            return check;
+        }
+
+        public static bool ComparePerson(this Person a, Person b)
+        {
+            return (a.Name == b.Name && a.PersistedFaceIds == b.PersistedFaceIds && a.PersonId == b.PersonId && a.UserData == b.UserData);
+        }
+
+        public static Person FindByID(this List<Person> a, Guid personID)
+        {
+            foreach(Person p in a)
+            {
+                if (p.PersonId == personID)
+                    return p;
+            }
+            return new Person();
+        }
+    }
+}
---
+++ HackathonEventAdmission/LiveCameraSample/MainWindow.xaml.cs
@@ -0,0 +1,369 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Cognitive Services: http://www.microsoft.com/cognitive
+// 
+// Microsoft Cognitive Services Github:
+// https://github.com/Microsoft/Cognitive
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Microsoft.ProjectOxford.Vision;
+using Newtonsoft.Json;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using VideoFrameAnalyzer;
+
+namespace LiveCameraSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        private FaceServiceClient _faceClient = null;
+        private readonly FrameGrabber<LiveCameraResult> _grabber = null;
+        private static readonly ImageEncodingParam[] s_jpegParams = {
+            new ImageEncodingParam(ImwriteFlags.JpegQuality, 60)
+        };
+        private readonly CascadeClassifier _localFaceDetector = new CascadeClassifier();
+        private LiveCameraResult _latestResultsToDisplay = null;
+        private AppMode _mode;
+        private DateTime _startTime;
+        private string personGroupID;
+        private List<Person> personsInGroup;
+
+        public enum AppMode
+        {
+            Faces,
+            Identify
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Create grabber. 
+            _grabber = new FrameGrabber<LiveCameraResult>();
+
+            // Set up a listener for when the client receives a new frame.
+            _grabber.NewFrameProvided += (s, e) =>
+            {             
+                // The callback may occur on a different thread, so we must use the
+                // MainWindow.Dispatcher when manipulating the UI. 
+                this.Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    // Display the image in the left pane.
+                    LeftImage.Source = e.Frame.Image.ToBitmapSource();
+                }));
+
+                // See if auto-stop should be triggered. 
+                if (Properties.Settings.Default.AutoStopEnabled && (DateTime.Now - _startTime) > Properties.Settings.Default.AutoStopTime)
+                {
+                    _grabber.StopProcessingAsync();
+                }
+            };
+
+            // Set up a listener for when the client receives a new result from an API call. 
+            _grabber.NewResultAvailable += (s, e) =>
+            {
+                this.Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    if (e.TimedOut)
+                    {
+                        MessageArea.Text = "API call timed out.";
+                    }
+                    else if (e.Exception != null)
+                    {
+                        string apiName = "";
+                        string message = e.Exception.Message;
+                        var faceEx = e.Exception as FaceAPIException;
+                        if (faceEx != null)
+                        {
+                            apiName = "Face";
+                            message = faceEx.ErrorMessage;
+                        }
+                        MessageArea.Text = string.Format("{0} API call failed on frame {1}. Exception: {2}", apiName, e.Frame.Metadata.Index, message);
+                    }
+                    else
+                    {
+                        _latestResultsToDisplay = e.Analysis;
+                        RightImage.Source = VisualizeResult(e.Frame);
+                    }
+                }));
+            };
+
+            // Create local face detector. 
+            _localFaceDetector.Load("Data/haarcascade_frontalface_alt2.xml");
+        }
+
+        /// <summary> Function which submits a frame to the Face API. </summary>
+        /// <param name="frame"> The video frame to submit. </param>
+        /// <returns> A <see cref="Task{LiveCameraResult}"/> representing the asynchronous API call,
+        ///     and containing the faces returned by the API. </returns>
+        private async Task<LiveCameraResult> FacesAnalysisFunction(VideoFrame frame)
+        {
+            // Encode image. 
+            var jpg = frame.Image.ToMemoryStream(".jpg", s_jpegParams);
+            // Submit image to API. 
+            var attrs = new List<FaceAttributeType> { FaceAttributeType.Age,
+                FaceAttributeType.Gender, FaceAttributeType.HeadPose };
+            var faces = await _faceClient.DetectAsync(jpg, returnFaceAttributes: attrs);
+            // Count the API call. 
+            Properties.Settings.Default.FaceAPICallCount++;
+            // Output. 
+            return new LiveCameraResult { Faces = faces };
+        }
+
+        private async Task<LiveCameraResult> IdentifyAnalysisFunction(VideoFrame frame)
+        {
+            List<Person> persons = new List<Person>();
+            //_timeStampQueue.Enqueue(DateTime.UtcNow);
+            var jpg = frame.Image.ToMemoryStream(".jpg", s_jpegParams);
+            var attrs = new List<FaceAttributeType> { FaceAttributeType.Age };
+            //await WaitCallLimitPerSecondAsync();
+            var faces = await _faceClient.DetectAsync(jpg, returnFaceAttributes: attrs);
+            Properties.Settings.Default.FaceAPICallCount++;
+            // Count the API call. 
+            var faceIDs = faces.Select(x => x.FaceId).ToArray();
+            Properties.Settings.Default.FaceAPICallCount++;
+            if(faceIDs.Length==0)
+            {
+                return new LiveCameraResult();
+            }
+            var personList = await _faceClient.IdentifyAsync(personGroupID, faceIDs);
+            Properties.Settings.Default.FaceAPICallCount++;
+            string[] celebNames = new string[0];
+            if (personList != null && personList[0] != null)
+            {
+                celebNames = new string[personList.Length];
+                for (int i = 0; i < personList.Length; i++)
+                {
+                    if (personList[i].Candidates.Length > 0)
+                    {
+                        var result = personsInGroup.FindByID(personList[i].Candidates[0].PersonId);
+                        celebNames[i] = result.Name;
+                        persons.Add(result);
+                    }
+                    else
+                    {
+                        celebNames[i] = "Unknown person";
+                        persons.Add(new Person());
+                    }
+                }
+            }
+
+            return new LiveCameraResult
+            {
+                Persons = persons.ToArray(),
+                Faces = faces,
+                CelebrityNames = celebNames 
+            };
+        }
+
+        private BitmapSource VisualizeResult(VideoFrame frame)
+        {
+            // Draw any results on top of the image. 
+            BitmapSource visImage = frame.Image.ToBitmapSource();
+
+            var result = _latestResultsToDisplay;
+
+            if (result != null)
+            {
+                // See if we have local face detections for this image.
+                var clientFaces = (OpenCvSharp.Rect[])frame.UserData;
+                if (clientFaces != null && result.Faces != null)
+                {
+                    // If so, then the analysis results might be from an older frame. We need to match
+                    // the client-side face detections (computed on this frame) with the analysis
+                    // results (computed on the older frame) that we want to display. 
+                    MatchAndReplaceFaceRectangles(result.Faces, clientFaces);
+                }
+
+                visImage = Visualization.DrawFaces(visImage, result.Faces, result.CelebrityNames);
+                visImage = Visualization.DrawTags(visImage, result.Tags);
+            }
+
+            return visImage;
+        }
+
+        /// <summary> Populate CameraList in the UI, once it is loaded. </summary>
+        /// <param name="sender"> Source of the event. </param>
+        /// <param name="e">      Routed event information. </param>
+        private void CameraList_Loaded(object sender, RoutedEventArgs e)
+        {
+            int numCameras = _grabber.GetNumCameras();
+
+            if (numCameras == 0)
+            {
+                MessageArea.Text = "No cameras found!";
+            }
+
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = Enumerable.Range(0, numCameras).Select(i => string.Format("Camera {0}", i + 1));
+            comboBox.SelectedIndex = 0;
+        }
+
+        /// <summary> Populate ModeList in the UI, once it is loaded. </summary>
+        /// <param name="sender"> Source of the event. </param>
+        /// <param name="e">      Routed event information. </param>
+        private void ModeList_Loaded(object sender, RoutedEventArgs e)
+        {
+            var modes = (AppMode[])Enum.GetValues(typeof(AppMode));
+
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = modes.Select(m => m.ToString());
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void ModeList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            var comboBox = sender as ComboBox;
+            var modes = (AppMode[])Enum.GetValues(typeof(AppMode));
+            _mode = modes[comboBox.SelectedIndex];
+            switch (_mode)
+            {
+                case AppMode.Faces:
+                    _grabber.AnalysisFunction = FacesAnalysisFunction;
+                    break;
+                case AppMode.Identify:
+                    _grabber.AnalysisFunction = IdentifyAnalysisFunction;
+                    break;
+                default:
+                    _grabber.AnalysisFunction = null;
+                    break;
+            }
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CameraList.HasItems)
+            {
+                MessageArea.Text = "No cameras found; cannot start processing";
+                return;
+            }
+
+            // Clean leading/trailing spaces in API keys. 
+            Properties.Settings.Default.FaceAPIKey = Properties.Settings.Default.FaceAPIKey.Trim();
+
+            // Create API clients. 
+            _faceClient = new FaceServiceClient(Properties.Settings.Default.FaceAPIKey, Properties.Settings.Default.FaceAPIHost);
+
+            // How often to analyze. 
+            _grabber.TriggerAnalysisOnInterval(Properties.Settings.Default.AnalysisInterval);
+
+            // Reset message. 
+            MessageArea.Text = "";
+
+            // Record start time, for auto-stop
+            _startTime = DateTime.Now;
+
+            personGroupID = Properties.Settings.Default.PersonGroupID.Trim();
+
+            var persons = await _faceClient.ListPersonsAsync(personGroupID);
+            personsInGroup = persons.ToList();
+
+            await _grabber.StartProcessingCameraAsync(CameraList.SelectedIndex);
+        }
+
+        private async void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            await _grabber.StopProcessingAsync();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsPanel.Visibility = 1 - SettingsPanel.Visibility;
+        }
+
+        private void SaveSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsPanel.Visibility = Visibility.Hidden;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        private Face CreateFace(FaceRectangle rect)
+        {
+            return new Face
+            {
+                FaceRectangle = new FaceRectangle
+                {
+                    Left = rect.Left,
+                    Top = rect.Top,
+                    Width = rect.Width,
+                    Height = rect.Height
+                }
+            };
+        }
+
+        private void MatchAndReplaceFaceRectangles(Face[] faces, OpenCvSharp.Rect[] clientRects)
+        {
+            // Use a simple heuristic for matching the client-side faces to the faces in the
+            // results. Just sort both lists left-to-right, and assume a 1:1 correspondence. 
+
+            // Sort the faces left-to-right. 
+            var sortedResultFaces = faces
+                .OrderBy(f => f.FaceRectangle.Left + 0.5 * f.FaceRectangle.Width)
+                .ToArray();
+
+            // Sort the clientRects left-to-right.
+            var sortedClientRects = clientRects
+                .OrderBy(r => r.Left + 0.5 * r.Width)
+                .ToArray();
+
+            // Assume that the sorted lists now corrrespond directly. We can simply update the
+            // FaceRectangles in sortedResultFaces, because they refer to the same underlying
+            // objects as the input "faces" array. 
+            for (int i = 0; i < Math.Min(faces.Length, clientRects.Length); i++)
+            {
+                // convert from OpenCvSharp rectangles
+                OpenCvSharp.Rect r = sortedClientRects[i];
+                sortedResultFaces[i].FaceRectangle = new FaceRectangle { Left = r.Left, Top = r.Top, Width = r.Width, Height = r.Height };
+            }
+        }
+    }
+}
