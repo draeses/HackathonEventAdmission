06b8e8fb48f21bce5d1b6b11dcdb0375d745a4dd
Demirtas Ilyas <Demirtas.Ilyas@capdemooutlook.onmicrosoft.com>
2017-11-24 10:05:59 +0000

    PersonEntity Done

---
+++ HackathonEventAdmission/LiveCameraSample/PersonEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveCameraSample
+{
+    class PersonEntity
+    {
+        public string FirstName { private set; get; }
+        public string LastName { private set; get; }
+        public string Status { private set; get; }
+        public string Role { private set; get; }
+        public DateTime Birthday { private set; get; }
+
+        public PersonEntity()
+        {
+        }
+
+        public PersonEntity(string firstName, string lastName, string status, string role, DateTime birthday)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Status = status;
+            Role = role;
+            Birthday = birthday;
+        }
+    }
+}
